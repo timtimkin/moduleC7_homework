02b029291793d812c7894d91ca90a683aa26c841
timtimkin <119056094+timtimkin@users.noreply.github.com>
2023-05-15 18:43:12 +0500

    index.test.js

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,6 +1,11 @@
 import { reverseString } from "../index.js";
 describe("reverseString function works", function() {
-    it("function reverse hello", function() {
+    it("function reverse hello and world", function() {
         expect(reverseString('hello')).toEqual('olleh');
+        expect(reverseString('world')).toEqual('dlrow');
+    });
+//делает ошибочную функцию
+    it("function reverse invalid wow", function() {
+        expect(reverseString('wow')).toEqual('ого');
     });
 });
